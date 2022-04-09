9a4b23b1738f4952ad4ff84847dacaae4db4d3f9
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-04-09 17:23:18 +0300

    new test

--- src/test/java/com/pankova/MainTest.java
+++ src/test/java/com/pankova/MainTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    public void test() {
+    public void success_find_max() {
+        Main main = new Main();
+        var array = new long [] {1, 2, 3, 10, 4, 5};
+        var expectedResult = 10L;
+        var actual = main.findMax(array);
+        assertEquals(expectedResult, actual);
+
+
     }
 }
