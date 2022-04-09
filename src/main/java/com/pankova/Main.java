8b6792a885bfd4feaf39c4d852286b80ff3eb4a2
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-04-09 16:26:04 +0300

    added github actions

--- src/main/java/com/pankova/Main.java
+++ src/main/java/com/pankova/Main.java
@@ -3,5 +3,7 @@ package com.pankova;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,world");
+        System.out.println("уау");
+        var1 :=1;
     }
 }
